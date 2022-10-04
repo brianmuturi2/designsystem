ab6c7e3966b0d680275235475747c813fa5f3945
brianmuturi2 <brianmuturi2@gmail.com>
2022-10-04 03:29:47 +0300

    feat(playground): added react playground

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import {Button} from '@designsystem/react'
+
+import '@designsystem/scss/lib/Button.css'
+
+ReactDOM.render(
+    <Button label={'Example Button'}/>,
+    document.querySelector('#root')
+)
