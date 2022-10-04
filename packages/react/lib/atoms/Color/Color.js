48a6da804fa260e29195ced1f66d76730e7a9bf3
brianmuturi2 <brianmuturi2@gmail.com>
2022-10-04 05:24:47 +0300

    feat(color|utilities): created color component, generated utility classes dynamically

---
+++ packages/react/lib/atoms/Color/Color.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Spacing from '../../foundation/Spacing.js';
+
+const Color = ({ hexCode, width = Spacing.sm, height = Spacing.sm }) => {
+    const dimensions = `dse-width-${width} dse-height-${height}`;
+    return (React.createElement("div", { className: dimensions, style: {
+            backgroundColor: hexCode,
+            width,
+            height
+        } }));
+};
+
+export { Color as default };
+//# sourceMappingURL=Color.js.map
